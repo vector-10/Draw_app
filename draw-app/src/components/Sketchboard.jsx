8c8656faaf26d4a8224ce723f647496d7bf51ac6
Vector-9 <duzieblaise10@gmail.com>
2023-11-14 06:40:25 +0100

    a drag and drop bug

--- draw-app/src/components/Sketchboard.jsx
+++ draw-app/src/components/Sketchboard.jsx
@@ -5,7 +5,8 @@ const generator = rough.generator();
 const WhiteBoard = () => {
   // State for managing drawing elements and interactions
   const [elements, setElements] = useState([]);
-  const [drawing, setDrawing] = useState(false);
+  const [selectedElement, setSelectedElement] = useState(null);
+  const [action, setAction] = useState("");
   const [tool, setTool] = useState("line");
   // UseLayoutEffect: Responsible for rendering drawing elements
   useLayoutEffect(() => {
@@ -30,32 +31,30 @@ const WhiteBoard = () => {
     }
   }, [elements]);
 
-
   const distance = (a, b) =>
- Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
-const getElementAtPosition = (x, y) => {
- // Iterate through each element in the 'elements' array
- return elements.find((element) => {
-  const { elementType, x1, y1, x2, y2 } = element;
-  // Depending on the element type (line or rectangle), perform checks
-  if (elementType === "rectangle") {
-   // Check if the cursor position (x, y) falls within the boundaries of the rectangle
-   const minX = Math.min(x1, x2);
-   const maxX = Math.max(x1, x2);
-   const minY = Math.min(y1, y2);
-   const maxY = Math.max(y1, y2);
-   return x >= minX && x <= maxX && y >= minY && y <= maxY;
-  } else {
-   // Check if the cursor is close enough to the line using a mathematical offset
-   const a = { x: x1, y: y1 };
-   const b = { x: x2, y: y2 };
-   const c = { x, y };
-   const offset = distance(a, b) - (distance(a, c) + distance(b, c));
-   return Math.abs(offset) < 1;
-  }
- });
-};
-
+    Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+  const getElementAtPosition = (x, y) => {
+    // Iterate through each element in the 'elements' array
+    return elements.find((element) => {
+      const { elementType, x1, y1, x2, y2 } = element;
+      // Depending on the element type (line or rectangle), perform checks
+      if (elementType === "rectangle") {
+        // Check if the cursor position (x, y) falls within the boundaries of the rectangle
+        const minX = Math.min(x1, x2);
+        const maxX = Math.max(x1, x2);
+        const minY = Math.min(y1, y2);
+        const maxY = Math.max(y1, y2);
+        return x >= minX && x <= maxX && y >= minY && y <= maxY;
+      } else {
+        // Check if the cursor is close enough to the line using a mathematical offset
+        const a = { x: x1, y: y1 };
+        const b = { x: x2, y: y2 };
+        const c = { x, y };
+        const offset = distance(a, b) - (distance(a, c) + distance(b, c));
+        return Math.abs(offset) < 1;
+      }
+    });
+  };
 
   // Function to create a new drawing element
   const createElement = (x1, y1, x2, y2) => {
@@ -70,14 +69,14 @@ const getElementAtPosition = (x, y) => {
     return { x1, y1, x2, y2, roughElement };
   };
   // Event handler for mouse down
-    const handleMouseDown = (e) => {
-      const { clientX, clientY } = e;
-      // Check if the current tool is "Selection"
-      if (tool === "selection") {
-       // Find the element at the clicked position
-       const element = getElementAtPosition(clientX, clientY);
-       // If an element is found
-       if (element) {
+  const handleMouseDown = (e) => {
+    const { clientX, clientY } = e;
+    // Check if the current tool is "Selection"
+    if (tool === "selection") {
+      // Find the element at the clicked position
+      const element = getElementAtPosition(clientX, clientY);
+      // If an element is found
+      if (element) {
         // Calculate the offset from the top-left corner of the element
         const offsetX = clientX - element.x1;
         const offsetY = clientY - element.y1;
@@ -85,37 +84,77 @@ const getElementAtPosition = (x, y) => {
         setSelectedElement({ ...element, offsetX, offsetY });
         // Set the action to "moving" to indicate that dragging is in progress
         setAction("moving");
-       }
-      } else {
-        setDrawing(true);
-        const { clientX, clientY } = e;
-        // Create a new drawing element when mouse down is detected
-        const element = createElement(clientX, clientY, clientX, clientY);
-        setElements((prevState) => [...prevState, element]);
       }
-     };
+    } else {
+      setAction("drawing");
+      const { clientX, clientY } = e;
+      // Create a new drawing element when mouse down is detected
+      const element = createElement(clientX, clientY, clientX, clientY);
+      setElements((prevState) => [...prevState, element]);
+    }
+  };
   // Event handler for mouse move
   const handleMouseMove = (e) => {
-    if (!drawing) return;
     const { clientX, clientY } = e;
-    // Find the index of the last element created during mouse down
-    const index = elements.length - 1;
-    const { x1, y1 } = elements[index];
-    // Update the element's coordinates for dynamic drawing
-    const UpdatedElement = createElement(x1, y1, clientX, clientY);
-    const elementsCopy = [...elements];
-    elementsCopy[index] = UpdatedElement;
-    setElements(elementsCopy);
+
+    // Check if the current tool is "Selection"
+    if (tool === "selection") {
+      // Determine cursor style based on whether the mouse is over an element
+      e.target.style.cursor = getElementAtPosition(clientX, clientY, elements)
+        ? "move"
+        : "default";
+    }
+
+    // Check the current action
+    if (action === "drawing") {
+      const index = elements.length - 1;
+      const { x1, y1 } = elements[index];
+      // Update the element's coordinates for dynamic drawing
+      const updatedElement = createElement(x1, y1, clientX, clientY);
+      const elementsCopy = [...elements];
+      elementsCopy[index] = updatedElement;
+      setElements(elementsCopy);
+    } else if (action === "moving") {
+      // If in "moving" action (dragging an element)
+      const { id, x1, x2, y1, y2, elementType, offsetX, offsetY } =
+        selectedElement || {};
+      const width = x2 - x1;
+      const height = y2 - y1;
+      // Calculate the new position of the dragged element
+      const newX = clientX - offsetX;
+      const newY = clientY - offsetY;
+      // Update the element's coordinates to perform dragging
+      const UpdatedElement = createElement(
+        id,
+        newX,
+        newY,
+        newX + width,
+        newY + height,
+        elementType
+      );
+      const elementsCopy = [...elements];
+      elementsCopy[id] = UpdatedElement;
+      setElements(elementsCopy);
+    }
   };
+
   // Event handler for mouse up
   const handleMouseUp = () => {
-    setDrawing(false);
+    setAction(false);
   };
   // Return JSX to render the collaborative canvas
   return (
     <>
       <div className="d-flex col-md-2 justify-content-center gap-1">
         <div className="d-flex gap-1 align-items-center">
+          <label htmlFor="selection">Drag n Drop</label>
+          <input
+            type="radio"
+            id="selection"
+            checked={tool === "selection"}
+            onChange={() => setTool("selection")}
+          />
+
           <label htmlFor="line">Line</label>
           <input
             type="radio"
@@ -132,7 +171,7 @@ const getElementAtPosition = (x, y) => {
           <input
             type="radio"
             name="tool"
-            id="rect"
+            id="rectangle"
             checked={tool === "rectangle"}
             value="rectangle"
             className="mt-1"
